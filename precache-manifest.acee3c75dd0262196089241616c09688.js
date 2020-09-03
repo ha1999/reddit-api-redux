4b4f1b6b67ee76b9d078f1d30c8924b0cb6b92ca
ha_ln <ha_ln@septeni-technology.jp>
2020-09-03 14:24:10 +0700

    Updates

---
+++ precache-manifest.acee3c75dd0262196089241616c09688.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e749ac0c0ce325d9d42c98bd81ba6ff",
+    "url": "/reddit-api-redux/index.html"
+  },
+  {
+    "revision": "512c982b1c010c9403e7",
+    "url": "/reddit-api-redux/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/reddit-api-redux/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reddit-api-redux/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512c982b1c010c9403e7",
+    "url": "/reddit-api-redux/static/js/main.23629b35.chunk.js"
+  },
+  {
+    "revision": "c126b407551c89093bdd",
+    "url": "/reddit-api-redux/static/js/runtime-main.f9e1fae8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reddit-api-redux/static/media/logo.5d5d9eef.svg"
+  }
+]);
